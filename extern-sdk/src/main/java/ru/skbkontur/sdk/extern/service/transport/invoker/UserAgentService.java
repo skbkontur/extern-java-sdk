d0848b355646a11846247766c9d0709079e62211
Останин Игорь Сергеевич <ostanin.igor@skbkontur.ru>
2018-04-03 16:11:00 +0500

    user agent `java-sdk` replaced by ${artifactId}

--- extern-sdk/src/main/java/ru/skbkontur/sdk/extern/service/transport/invoker/UserAgentService.java
+++ extern-sdk/src/main/java/ru/skbkontur/sdk/extern/service/transport/invoker/UserAgentService.java
@@ -3,20 +3,14 @@ package ru.skbkontur.sdk.extern.service.transport.invoker;
 
 public class UserAgentService {
 
-    public static final String SDK = "java-sdk";
+    public static final String USER_AGENT_STRING = getUserAgentString();
 
-    public static final String SDK_VERSION = getSdkVersion();
-
-    public static final String SDK_VERSION_UNDEFINED = "undefined";
-
-    public static final String USER_AGENT_STRING = SDK + "/" + SDK_VERSION;
-
-    private static String getSdkVersion() {
-        String implementationVersion = UserAgentService.class.getPackage().getImplementationVersion();
-        if (implementationVersion == null) {
-            implementationVersion = SDK_VERSION_UNDEFINED;
-        }
-        return implementationVersion;
+    private static String getUserAgentString() {
+        Package aPackage = UserAgentService.class.getPackage();
+        String version = aPackage.getImplementationVersion();
+        String title = aPackage.getImplementationTitle();
+        return title + "/" + version;
     }
+
 
 }
