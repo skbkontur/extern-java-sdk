72549e4cdc9e282c06dd33638d61cfa633f58050
Ostanin Igor <i_o@rtural.ru>
2018-04-17 12:04:07 +0500

    License bumped to 2018 and included in all files

--- extern-sdk/src/main/java/ru/skbkontur/sdk/extern/service/transport/adaptors/QueryContext.java
+++ extern-sdk/src/main/java/ru/skbkontur/sdk/extern/service/transport/adaptors/QueryContext.java
@@ -1,8 +1,27 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * MIT License
+ *
+ * Copyright (c) 2018 SKB Kontur
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
+
 package ru.skbkontur.sdk.extern.service.transport.adaptors;
 
 import com.google.gson.GsonBuilder;
@@ -137,19 +156,6 @@ public class QueryContext<R> implements Serializable {
         return serviceError;
     }
 
-    public QueryContext<R> setServiceError(String message) {
-        return setServiceError(new ServiceErrorImpl(ServiceError.ErrorCode.business, message, 0, null, null));
-    }
-
-    public QueryContext<R> setServiceError(QueryContext<?> queryContext) {
-        this.result = null;
-        this.serviceError = queryContext.serviceError;
-        if (serviceError.getResponseCode() == HttpsURLConnection.HTTP_UNAUTHORIZED) {
-            set(SESSION_ID, null);
-        }
-        return this;
-    }
-
     public QueryContext<R> setServiceError(ServiceError serviceError) {
         this.result = null;
         this.serviceError = serviceError;
@@ -161,6 +167,19 @@ public class QueryContext<R> implements Serializable {
 
     public QueryContext<R> setServiceError(ApiException x) {
         return setServiceError(ErrorCode.server, x.getMessage(), x.getCode(), x.getResponseHeaders(), x.getResponseBody());
+    }
+
+    public QueryContext<R> setServiceError(String message) {
+        return setServiceError(new ServiceErrorImpl(ServiceError.ErrorCode.business, message, 0, null, null));
+    }
+
+    public QueryContext<R> setServiceError(QueryContext<?> queryContext) {
+        this.result = null;
+        this.serviceError = queryContext.serviceError;
+        if (serviceError.getResponseCode() == HttpsURLConnection.HTTP_UNAUTHORIZED) {
+            set(SESSION_ID, null);
+        }
+        return this;
     }
 
     public QueryContext<R> setServiceError(ServiceError.ErrorCode errorCode, String message, int code, Map<String, List<String>> responseHeaders, String responseBody) {
