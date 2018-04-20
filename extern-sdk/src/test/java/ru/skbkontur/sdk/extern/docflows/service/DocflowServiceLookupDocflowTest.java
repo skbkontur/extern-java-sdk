054c4af198b1eb65ea3508881a401db40311899a
Pavlenko Mikhail <kosolapiy@bk.ru>
2018-04-20 13:30:09 +0300

    docflows tests

---
+++ extern-sdk/src/test/java/ru/skbkontur/sdk/extern/docflows/service/DocflowServiceLookupDocflowTest.java
@@ -0,0 +1,203 @@
+package ru.skbkontur.sdk.extern.docflows.service;
+
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertNull;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ru.skbkontur.sdk.extern.ExternEngine;
+import ru.skbkontur.sdk.extern.common.ResponseData;
+import ru.skbkontur.sdk.extern.common.StandardObjects;
+import ru.skbkontur.sdk.extern.common.StandardValues;
+import ru.skbkontur.sdk.extern.common.TestServlet;
+import ru.skbkontur.sdk.extern.docflows.DocflowsValidator;
+import ru.skbkontur.sdk.extern.event.AuthenticationListener;
+import ru.skbkontur.sdk.extern.model.Docflow;
+import ru.skbkontur.sdk.extern.providers.AuthenticationProvider;
+import ru.skbkontur.sdk.extern.providers.ServiceError;
+import ru.skbkontur.sdk.extern.service.transport.adaptors.QueryContext;
+
+public class DocflowServiceLookupDocflowTest {
+
+  private static ExternEngine engine;
+  private static Server server;
+
+  private final static String DOCFLOW = "\"id\": \"" + StandardValues.ID + "\"," +
+      "\"type\": \"urn:nss:nid\"," +
+      "\"status\": \"urn:nss:nid\"," +
+      "\"send-date\": \"" + StandardValues.DATE + "\"," +
+      "\"last-change-date\": \"" + StandardValues.DATE + "\"";
+
+
+  @BeforeClass
+  public static void startJetty() throws Exception {
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.setContextPath("/");
+    context.addServlet(TestServlet.class, "/docflows/*");
+    server = new Server(8080);
+    server.setHandler(context);
+    server.start();
+  }
+
+  @AfterClass
+  public static void stopJetty() {
+    try {
+      server.stop();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @BeforeClass
+  public static void setUpClass() {
+    engine = new ExternEngine();
+    engine.setServiceBaseUriProvider(() -> "http://localhost:8080/docflows");
+    engine.setAccountProvider(UUID::randomUUID);
+    engine.setApiKeyProvider(() -> UUID.randomUUID().toString());
+    engine.setAuthenticationProvider(
+        new AuthenticationProvider() {
+          @Override
+          public QueryContext<String> sessionId() {
+            return new QueryContext<String>().setResult("1", QueryContext.SESSION_ID);
+          }
+
+          @Override
+          public String authPrefix() {
+            return "auth.sid ";
+          }
+
+          @Override
+          public void addAuthenticationListener(AuthenticationListener authListener) {
+          }
+
+          @Override
+          public void removeAuthenticationListener(AuthenticationListener authListener) {
+          }
+
+          @Override
+          public void raiseUnauthenticated(ServiceError x) {
+          }
+        });
+
+    engine.configureServices();
+  }
+
+  @Test
+  public void testLookupDocflow_Empty() {
+    ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
+    ResponseData.INSTANCE.setResponseMessage("{}");
+    assertNotNull("Docflow must not be null!", getDocflow());
+    assertNotNull("Docflow must not be null!", getDocflowAsync());
+  }
+
+  @Test
+  public void testLookupDocflow_Docflow() {
+    ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
+    ResponseData.INSTANCE.setResponseMessage(String.format("{%s}", DOCFLOW));
+    DocflowsValidator.validateDocflow(getDocflow(), false, false, false);
+    DocflowsValidator.validateDocflow(getDocflowAsync(), false, false, false);
+  }
+
+  @Test
+  public void testLookupDocflow_Docflow_Description() {
+    ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
+    ResponseData.INSTANCE.setResponseMessage(String.format("{%s,\"description\": {}}", DOCFLOW));
+    DocflowsValidator.validateDocflow(getDocflow(), true, false, false);
+    DocflowsValidator.validateDocflow(getDocflowAsync(), true, false, false);
+  }
+
+  @Test
+  public void testLookupDocflow_Docflow_Documents() {
+    ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
+    ResponseData.INSTANCE.setResponseMessage(String.format("{%s," +
+        "\"description\": {}," +
+        "\"documents\": [{" +
+        "   \"id\": \"%s\"" +
+        "}]" +
+        "}", DOCFLOW, StandardValues.ID));
+    DocflowsValidator.validateDocflow(getDocflow(), true, true, false);
+    DocflowsValidator.validateDocflow(getDocflowAsync(), true, true, false);
+  }
+
+  @Test
+  public void testLookupDocflow_Docflow_Links() {
+    ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
+    ResponseData.INSTANCE.setResponseMessage("{" +
+        DOCFLOW + "," +
+        "  \"description\": {}," +
+        "  \"documents\": [{\"id\": \"" + StandardValues.ID + "\"}]," +
+        "  \"links\": [" + StandardObjects.LINK + "]" +
+        "}");
+    DocflowsValidator.validateDocflow(getDocflow(), true, true, true);
+    DocflowsValidator.validateDocflow(getDocflowAsync(), true, true, true);
+  }
+
+  @Test
+  public void testLookupDocflow_BAD_REQUEST() {
+    ResponseData.INSTANCE.setResponseCode(SC_BAD_REQUEST); // 400
+    checkResponseCode(SC_BAD_REQUEST);
+  }
+
+  @Test
+  public void testLookupDocflow_UNAUTHORIZED() {
+    ResponseData.INSTANCE.setResponseCode(SC_UNAUTHORIZED); // 401
+    checkResponseCode(SC_UNAUTHORIZED);
+  }
+
+  @Test
+  public void testLookupDocflow_FORBIDDEN() {
+    ResponseData.INSTANCE.setResponseCode(SC_FORBIDDEN); // 403
+    checkResponseCode(SC_FORBIDDEN);
+  }
+
+  @Test
+  public void testLookupDocflow_NOT_FOUND() {
+    ResponseData.INSTANCE.setResponseCode(SC_NOT_FOUND); // 404
+    checkResponseCode(SC_NOT_FOUND);
+  }
+
+  @Test
+  public void testLookupDocflow_INTERNAL_SERVER_ERROR() {
+    ResponseData.INSTANCE.setResponseCode(SC_INTERNAL_SERVER_ERROR); // 500
+    checkResponseCode(SC_INTERNAL_SERVER_ERROR);
+  }
+
+  private void checkResponseCode(int code) {
+    QueryContext<Object> queryContext = new QueryContext<>();
+    queryContext.setDocflowId(StandardValues.ID);
+    QueryContext<Docflow> docflowQueryContext = engine.getDocflowService()
+        .lookupDocflow(queryContext);
+    assertNull("docflow must be null!", docflowQueryContext.get());
+    assertNotNull("ServiceError must not be null!", docflowQueryContext.getServiceError());
+    assertEquals("Response code is wrong!", code,
+        docflowQueryContext.getServiceError().getResponseCode());
+  }
+
+  private Docflow getDocflow() {
+    QueryContext<Docflow> queryContext = new QueryContext<>();
+    queryContext.setDocflowId(StandardValues.ID);
+    return engine.getDocflowService().lookupDocflow(queryContext).get();
+  }
+
+  private Docflow getDocflowAsync() {
+    try {
+      return engine.getDocflowService().lookupDocflowAsync(StandardValues.ID).get().get();
+    } catch (InterruptedException | ExecutionException e) {
+      fail();
+      return null;
+    }
+  }
+}
---
+++ extern-sdk/src/test/java/ru/skbkontur/sdk/extern/docflows/service/DocflowServiceLookupDocumentTest.java
@@ -0,0 +1,229 @@
+package ru.skbkontur.sdk.extern.docflows.service;
+
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
+import static junit.framework.TestCase.fail;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ru.skbkontur.sdk.extern.ExternEngine;
+import ru.skbkontur.sdk.extern.common.ResponseData;
+import ru.skbkontur.sdk.extern.common.StandardObjects;
+import ru.skbkontur.sdk.extern.common.StandardValues;
+import ru.skbkontur.sdk.extern.common.TestServlet;
+import ru.skbkontur.sdk.extern.docflows.DocflowsValidator;
+import ru.skbkontur.sdk.extern.event.AuthenticationListener;
+import ru.skbkontur.sdk.extern.model.Docflow;
+import ru.skbkontur.sdk.extern.model.Document;
+import ru.skbkontur.sdk.extern.providers.AuthenticationProvider;
+import ru.skbkontur.sdk.extern.providers.ServiceError;
+import ru.skbkontur.sdk.extern.service.transport.adaptors.QueryContext;
+
+public class DocflowServiceLookupDocumentTest {
+
+  private static ExternEngine engine;
+  private static Server server;
+
+  private final static String DOCUMENT_DESCRIPTION = "{\"type\": \"urn:nss:nid\"," +
+      "\"filename\": \"string\"," +
+      "\"content-type\": \"string\"}";
+
+  @BeforeClass
+  public static void startJetty() throws Exception {
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.setContextPath("/");
+    context.addServlet(TestServlet.class, "/docflows/*");
+    server = new Server(8080);
+    server.setHandler(context);
+    server.start();
+  }
+
+  @AfterClass
+  public static void stopJetty() {
+    try {
+      server.stop();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @BeforeClass
+  public static void setUpClass() {
+    engine = new ExternEngine();
+    engine.setServiceBaseUriProvider(() -> "http://localhost:8080/docflows");
+    engine.setAccountProvider(UUID::randomUUID);
+    engine.setApiKeyProvider(() -> UUID.randomUUID().toString());
+    engine.setAuthenticationProvider(
+        new AuthenticationProvider() {
+          @Override
+          public QueryContext<String> sessionId() {
+            return new QueryContext<String>().setResult("1", QueryContext.SESSION_ID);
+          }
+
+          @Override
+          public String authPrefix() {
+            return "auth.sid ";
+          }
+
+          @Override
+          public void addAuthenticationListener(AuthenticationListener authListener) {
+          }
+
+          @Override
+          public void removeAuthenticationListener(AuthenticationListener authListener) {
+          }
+
+          @Override
+          public void raiseUnauthenticated(ServiceError x) {
+          }
+        });
+
+    engine.configureServices();
+  }
+
+  @Test
+  public void testLookupDocument_Empty() {
+    ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
+    ResponseData.INSTANCE.setResponseMessage("{}");
+    assertNotNull("Document must not be null!", getDocument());
+    assertNotNull("Document must not be null!", getDocumentAsync());
+  }
+
+  @Test
+  public void testLookupDocument_Document() {
+    ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
+    ResponseData.INSTANCE.setResponseMessage(String.format("{\"id\": \"%s\"}", StandardValues.ID));
+    DocflowsValidator.validateDocument(getDocument(), false, false, false, false);
+    DocflowsValidator.validateDocument(getDocumentAsync(), false, false, false, false);
+  }
+
+  @Test
+  public void testLookupDocument_Document_Description() {
+    ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
+    ResponseData.INSTANCE.setResponseMessage("{" +
+        "\"id\": \"" + StandardValues.ID + "\"," +
+        "\"description\": " + DOCUMENT_DESCRIPTION +
+        "}");
+    DocflowsValidator.validateDocument(getDocument(), true, false, false, false);
+    DocflowsValidator.validateDocument(getDocumentAsync(), true, false, false, false);
+  }
+
+  @Test
+  public void testLookupDocument_Document_WithContent() {
+    ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
+    ResponseData.INSTANCE.setResponseMessage("{" +
+        "\"id\": \"" + StandardValues.ID + "\"," +
+        "\"description\": " + DOCUMENT_DESCRIPTION + "," +
+        "\"content\": {\n" +
+        "  \"decrypted\": " + StandardObjects.LINK + "," +
+        "  \"encrypted\": " + StandardObjects.LINK +
+        "}" +
+        "}");
+    DocflowsValidator.validateDocument(getDocument(), true, true, false, false);
+    DocflowsValidator.validateDocument(getDocumentAsync(), true, true, false, false);
+  }
+
+  @Test
+  public void testLookupDocument_Document_Signature() {
+    ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
+    ResponseData.INSTANCE.setResponseMessage("{" +
+        "\"id\": \"" + StandardValues.ID + "\"," +
+        "\"description\": " + DOCUMENT_DESCRIPTION + "," +
+        "\"content\": {\n" +
+        "  \"decrypted\": " + StandardObjects.LINK + "," +
+        "  \"encrypted\": " + StandardObjects.LINK +
+        "}," +
+        "\"signatures\": [{\"id\": \"" + StandardValues.ID + "\"}]" +
+        "}");
+    DocflowsValidator.validateDocument(getDocument(), true, true, true, false);
+    DocflowsValidator.validateDocument(getDocumentAsync(), true, true, true, false);
+  }
+
+  @Test
+  public void testLookupDocument_Document_Links() {
+    ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
+    ResponseData.INSTANCE.setResponseMessage("{" +
+        "\"id\": \"" + StandardValues.ID + "\"," +
+        "\"description\": " + DOCUMENT_DESCRIPTION + "," +
+        "\"content\": {\n" +
+        "  \"decrypted\": " + StandardObjects.LINK + "," +
+        "  \"encrypted\": " + StandardObjects.LINK +
+        "}," +
+        "\"signatures\": [{\"id\": \"" + StandardValues.ID + "\"}]," +
+        "\"links\": [" + StandardObjects.LINK + "]" +
+        "}");
+    DocflowsValidator.validateDocument(getDocument(), true, true, true, true);
+    DocflowsValidator.validateDocument(getDocumentAsync(), true, true, true, true);
+  }
+
+  @Test
+  public void testGetDocflows_BAD_REQUEST() {
+    ResponseData.INSTANCE.setResponseCode(SC_BAD_REQUEST); // 400
+    checkResponseCode(SC_BAD_REQUEST);
+  }
+
+  @Test
+  public void testGetDocflows_UNAUTHORIZED() {
+    ResponseData.INSTANCE.setResponseCode(SC_UNAUTHORIZED); // 401
+    checkResponseCode(SC_UNAUTHORIZED);
+  }
+
+  @Test
+  public void testGetDocflows_FORBIDDEN() {
+    ResponseData.INSTANCE.setResponseCode(SC_FORBIDDEN); // 403
+    checkResponseCode(SC_FORBIDDEN);
+  }
+
+  @Test
+  public void testGetDocflows_NOT_FOUND() {
+    ResponseData.INSTANCE.setResponseCode(SC_NOT_FOUND); // 404
+    checkResponseCode(SC_NOT_FOUND);
+  }
+
+  @Test
+  public void testGetDocflows_INTERNAL_SERVER_ERROR() {
+    ResponseData.INSTANCE.setResponseCode(SC_INTERNAL_SERVER_ERROR); // 500
+    checkResponseCode(SC_INTERNAL_SERVER_ERROR);
+  }
+
+  private void checkResponseCode(int code) {
+    QueryContext<Docflow> queryContext = new QueryContext<>();
+    queryContext.setDocflowId(StandardValues.ID);
+    queryContext.setDocumentId(StandardValues.ID);
+    QueryContext<Document> documentQueryContext = engine.getDocflowService()
+        .lookupDocument(queryContext);
+    assertNull("document must be null!", documentQueryContext.get());
+    ServiceError serviceError = documentQueryContext.getServiceError();
+    assertNotNull("ServiceError must not be null!", serviceError);
+    assertEquals("Response code is wrong!", code, serviceError.getResponseCode());
+  }
+
+  private Document getDocument() {
+    QueryContext<Docflow> queryContext = new QueryContext<>();
+    queryContext.setDocflowId(StandardValues.ID);
+    queryContext.setDocumentId(StandardValues.ID);
+    return engine.getDocflowService().lookupDocument(queryContext).get();
+  }
+
+  private Document getDocumentAsync() {
+    try {
+      return engine.getDocflowService().lookupDocumentAsync(StandardValues.ID, StandardValues.ID)
+          .get().get();
+    } catch (InterruptedException | ExecutionException e) {
+      fail();
+      return null;
+    }
+  }
+}
