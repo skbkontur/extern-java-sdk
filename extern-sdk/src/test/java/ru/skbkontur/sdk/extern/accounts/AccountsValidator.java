5e5450f6626d182c44b16b41e92b0c762b09a75f
Pavlenko Mikhail <kosolapiy@bk.ru>
2018-04-23 11:04:55 +0300

    remove warnings and cleanup code

--- extern-sdk/src/test/java/ru/skbkontur/sdk/extern/accounts/AccountsValidator.java
+++ extern-sdk/src/test/java/ru/skbkontur/sdk/extern/accounts/AccountsValidator.java
@@ -1,19 +1,43 @@
 package ru.skbkontur.sdk.extern.accounts;
 
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+
 import ru.skbkontur.sdk.extern.common.StandardObjectsValidator;
 import ru.skbkontur.sdk.extern.model.Account;
 import ru.skbkontur.sdk.extern.model.AccountList;
 
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertNotNull;
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 SKB Kontur
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author Mikhail Pavlenko
+ */
 
 public class AccountsValidator {
+
     public static void validateAccountList(AccountList accountList, boolean withAccounts) {
         assertNotNull("accountList must not be null!", accountList);
         assertEquals("Skip is wrong!", 0, accountList.getSkip().intValue());
         assertEquals("Take is wrong!", 0, accountList.getTake().intValue());
         assertEquals("TotalCount is wrong!", 0, accountList.getTotalCount().intValue());
-        if(withAccounts){
+        if (withAccounts) {
             StandardObjectsValidator.validateNotEmptyList(accountList.getAccounts(), "Accounts");
             validateAccount(accountList.getAccounts().get(0), false);
         } else {
@@ -27,7 +51,7 @@ public class AccountsValidator {
         assertEquals("Inn is wrong!", "string", account.getInn());
         assertEquals("Kpp is wrong!", "string", account.getKpp());
         assertEquals("OrganizationName is wrong!", "string", account.getOrganizationName());
-        if(withLinks){
+        if (withLinks) {
             StandardObjectsValidator.validateNotEmptyList(account.getLinks(), "Links");
             StandardObjectsValidator.validateLink(account.getLinks().get(0));
         } else {
--- extern-sdk/src/test/java/ru/skbkontur/sdk/extern/accounts/service/AccountServiceGetAccountTest.java
+++ extern-sdk/src/test/java/ru/skbkontur/sdk/extern/accounts/service/AccountServiceGetAccountTest.java
@@ -1,5 +1,19 @@
 package ru.skbkontur.sdk.extern.accounts.service;
 
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
+import static junit.framework.TestCase.fail;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.junit.AfterClass;
@@ -18,14 +32,26 @@ import ru.skbkontur.sdk.extern.providers.AuthenticationProvider;
 import ru.skbkontur.sdk.extern.providers.ServiceError;
 import ru.skbkontur.sdk.extern.service.transport.adaptors.QueryContext;
 
-import javax.servlet.http.HttpServletResponse;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-
-import static javax.servlet.http.HttpServletResponse.*;
-import static junit.framework.TestCase.*;
-
 /**
+ * MIT License
+ *
+ * Copyright (c) 2018 SKB Kontur
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
  * @author Mikhail Pavlenko
  */
 
