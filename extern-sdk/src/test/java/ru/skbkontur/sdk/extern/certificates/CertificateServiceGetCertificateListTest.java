3ee25fb5639a59e489e1fffd8c3ee53c8d623c13
Pavlenko Mikhail <kosolapiy@bk.ru>
2018-04-23 11:06:44 +0300

    remove warnings and cleanup code

--- extern-sdk/src/test/java/ru/skbkontur/sdk/extern/certificates/CertificateServiceGetCertificateListTest.java
+++ extern-sdk/src/test/java/ru/skbkontur/sdk/extern/certificates/CertificateServiceGetCertificateListTest.java
@@ -1,5 +1,20 @@
 package ru.skbkontur.sdk.extern.certificates;
 
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
+import static junit.framework.TestCase.fail;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.junit.AfterClass;
@@ -16,22 +31,38 @@ import ru.skbkontur.sdk.extern.providers.AuthenticationProvider;
 import ru.skbkontur.sdk.extern.providers.ServiceError;
 import ru.skbkontur.sdk.extern.service.transport.adaptors.QueryContext;
 
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 SKB Kontur
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author Mikhail Pavlenko
+ */
 
-import static javax.servlet.http.HttpServletResponse.*;
-import static junit.framework.TestCase.*;
+public class CertificateServiceGetCertificateListTest {
 
-public class CertificateServiceTest {
     private static ExternEngine engine;
     private static Server server;
 
     private final static String CERTIFICATE_LIST = "" +
-            "\"total-count\": 0," +
-            "\"page-index\": 0," +
-            "\"page-size\": 0";
+        "\"total-count\": 0," +
+        "\"page-index\": 0," +
+        "\"page-size\": 0";
 
     @BeforeClass
     public static void startJetty() throws Exception {
@@ -59,29 +90,29 @@ public class CertificateServiceTest {
         engine.setAccountProvider(UUID::randomUUID);
         engine.setApiKeyProvider(() -> UUID.randomUUID().toString());
         engine.setAuthenticationProvider(
-                new AuthenticationProvider() {
-                    @Override
-                    public QueryContext<String> sessionId() {
-                        return new QueryContext<String>().setResult("1", QueryContext.SESSION_ID);
-                    }
+            new AuthenticationProvider() {
+                @Override
+                public QueryContext<String> sessionId() {
+                    return new QueryContext<String>().setResult("1", QueryContext.SESSION_ID);
+                }
 
-                    @Override
-                    public String authPrefix() {
-                        return "auth.sid ";
-                    }
+                @Override
+                public String authPrefix() {
+                    return "auth.sid ";
+                }
 
-                    @Override
-                    public void addAuthenticationListener(AuthenticationListener authListener) {
-                    }
+                @Override
+                public void addAuthenticationListener(AuthenticationListener authListener) {
+                }
 
-                    @Override
-                    public void removeAuthenticationListener(AuthenticationListener authListener) {
-                    }
+                @Override
+                public void removeAuthenticationListener(AuthenticationListener authListener) {
+                }
 
-                    @Override
-                    public void raiseUnauthenticated(ServiceError x) {
-                    }
-                });
+                @Override
+                public void raiseUnauthenticated(ServiceError x) {
+                }
+            });
 
         engine.configureServices();
 
@@ -91,14 +122,17 @@ public class CertificateServiceTest {
     public void testGetCertificateList_Empty() {
         ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
         ResponseData.INSTANCE.setResponseMessage("{}");
-        assertNotNull("Certificates must not be null!", engine.getCertificateService().getCertificateList(new QueryContext<CertificateList>()).get());
+        assertNotNull("Certificates must not be null!",
+            engine.getCertificateService().getCertificateList(new QueryContext<CertificateList>())
+                .get());
     }
 
     @Test
     public void testGetCertificateListAsync_Empty() {
         ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
         ResponseData.INSTANCE.setResponseMessage("{}");
-        CompletableFuture<QueryContext<CertificateList>> queryContextAsync = engine.getCertificateService().getCertificateListAsync();
+        CompletableFuture<QueryContext<CertificateList>> queryContextAsync = engine
+            .getCertificateService().getCertificateListAsync();
         try {
             assertNotNull("Certificates must not be null!", queryContextAsync.get().get());
         } catch (InterruptedException | ExecutionException e) {
@@ -110,9 +144,11 @@ public class CertificateServiceTest {
     public void testGetCertificateList() {
         ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
         ResponseData.INSTANCE.setResponseMessage("{" + CERTIFICATE_LIST + "}");
-        CertificateList certificateList = engine.getCertificateService().getCertificateList(new QueryContext<CertificateList>()).get();
+        CertificateList certificateList = engine.getCertificateService()
+            .getCertificateList(new QueryContext<CertificateList>()).get();
         CertificatesValidator.validateCertificateList(certificateList);
-        StandardObjectsValidator.validateEmptyList(certificateList.getCertificates(), "Certificates");
+        StandardObjectsValidator
+            .validateEmptyList(certificateList.getCertificates(), "Certificates");
     }
 
     @Test
@@ -120,9 +156,11 @@ public class CertificateServiceTest {
         ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
         ResponseData.INSTANCE.setResponseMessage("{" + CERTIFICATE_LIST + "}");
         try {
-            CertificateList certificateList = engine.getCertificateService().getCertificateListAsync().get().get();
+            CertificateList certificateList = engine.getCertificateService()
+                .getCertificateListAsync().get().get();
             CertificatesValidator.validateCertificateList(certificateList);
-            StandardObjectsValidator.validateEmptyList(certificateList.getCertificates(), "Certificates");
+            StandardObjectsValidator
+                .validateEmptyList(certificateList.getCertificates(), "Certificates");
         } catch (InterruptedException | ExecutionException e) {
             fail();
         }
@@ -132,18 +170,19 @@ public class CertificateServiceTest {
     public void testGetCertificates_Certificates() {
         ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
         ResponseData.INSTANCE.setResponseMessage("{" +
-                CERTIFICATE_LIST + "," +
-                "\"certificates\": [{" +
-                "\"fio\": \"string\"," +
-                "\"inn\": \"string\"," +
-                "\"kpp\": \"string\"," +
-                "\"is-valid\": true," +
-                "\"is-cloud\": true," +
-                "\"is-qualified\": true," +
-                "\"content\": \"string\"}]" +
-                "}");
+            CERTIFICATE_LIST + "," +
+            "\"certificates\": [{" +
+            "\"fio\": \"string\"," +
+            "\"inn\": \"string\"," +
+            "\"kpp\": \"string\"," +
+            "\"is-valid\": true," +
+            "\"is-cloud\": true," +
+            "\"is-qualified\": true," +
+            "\"content\": \"string\"}]" +
+            "}");
         //
-        CertificateList certificateList = engine.getCertificateService().getCertificateList(new QueryContext<CertificateList>()).get();
+        CertificateList certificateList = engine.getCertificateService()
+            .getCertificateList(new QueryContext<CertificateList>()).get();
         CertificatesValidator.validateCertificateList(certificateList);
         List<Certificate> certificates = certificateList.getCertificates();
         StandardObjectsValidator.validateNotEmptyList(certificates, "Certificates");
@@ -154,18 +193,19 @@ public class CertificateServiceTest {
     public void testGetCertificatesAsync_Certificates() {
         ResponseData.INSTANCE.setResponseCode(SC_OK); // 200
         ResponseData.INSTANCE.setResponseMessage("{" +
-                CERTIFICATE_LIST + "," +
-                "\"certificates\": [{" +
-                "\"fio\": \"string\"," +
-                "\"inn\": \"string\"," +
-                "\"kpp\": \"string\"," +
-                "\"is-valid\": true," +
-                "\"is-cloud\": true," +
-                "\"is-qualified\": true," +
-                "\"content\": \"string\"}]" +
-                "}");
+            CERTIFICATE_LIST + "," +
+            "\"certificates\": [{" +
+            "\"fio\": \"string\"," +
+            "\"inn\": \"string\"," +
+            "\"kpp\": \"string\"," +
+            "\"is-valid\": true," +
+            "\"is-cloud\": true," +
+            "\"is-qualified\": true," +
+            "\"content\": \"string\"}]" +
+            "}");
         try {
-            CertificateList certificateList = engine.getCertificateService().getCertificateListAsync().get().get();
+            CertificateList certificateList = engine.getCertificateService()
+                .getCertificateListAsync().get().get();
             CertificatesValidator.validateCertificateList(certificateList);
             List<Certificate> certificates = certificateList.getCertificates();
             StandardObjectsValidator.validateNotEmptyList(certificates, "Certificates");
@@ -206,7 +246,8 @@ public class CertificateServiceTest {
     }
 
     private void checkResponseCode(int code) {
-        QueryContext<CertificateList> queryContext = engine.getCertificateService().getCertificateList(new QueryContext<CertificateList>());
+        QueryContext<CertificateList> queryContext = engine.getCertificateService()
+            .getCertificateList(new QueryContext<CertificateList>());
         assertNull("certificateList must be null!", queryContext.get());
         ServiceError serviceError = queryContext.getServiceError();
         assertNotNull("ServiceError must not be null!", serviceError);
