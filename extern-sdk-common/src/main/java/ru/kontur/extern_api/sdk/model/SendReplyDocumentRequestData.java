25963937c25f9a42b6269fc78d1a715bee1492e7
EvgeniaRatner <eu.ratner@yandex.ru>
2018-08-20 13:49:15 +0500

    Update SendReplyDocumentRequestData.java

--- extern-sdk-common/src/main/java/ru/kontur/extern_api/sdk/model/SendReplyDocumentRequestData.java
+++ extern-sdk-common/src/main/java/ru/kontur/extern_api/sdk/model/SendReplyDocumentRequestData.java
@@ -4,7 +4,6 @@ import com.google.gson.annotations.SerializedName;
 
 public class SendReplyDocumentRequestData {
 
-    @SerializedName("senderIp")
     private String senderIp = null;
 
     public SendReplyDocumentRequestData senderIp(String senderIp) {
