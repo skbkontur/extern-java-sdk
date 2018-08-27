80055552473f3a7f45fa77c1b04bc445d6604740
andection <andection@skbkontur.ru>
2018-08-27 15:44:51 +0500

    fixed content urls

--- extern-sdk-httpclient-adaptor/src/main/java/ru/kontur/extern_api/sdk/service/transport/adaptor/httpclient/api/DraftsApi.java
+++ extern-sdk-httpclient-adaptor/src/main/java/ru/kontur/extern_api/sdk/service/transport/adaptor/httpclient/api/DraftsApi.java
@@ -309,7 +309,7 @@ public class DraftsApi extends RestApi {
      * @return ApiResponse&lt;String&gt;
      * @throws ApiException transport exception
      */
-    @Path("/v1/{accountId}/drafts/{draftId}/documents/{documentId}/content/decrypted")
+    @Path("/v1/{accountId}/drafts/{draftId}/documents/{documentId}/decrypted-content")
     @GET
     @Consumes("application/json; charset=utf-8")
     public ApiResponse<String> getDecryptedDocumentContent(@PathParam("accountId") String accountId,
@@ -330,7 +330,7 @@ public class DraftsApi extends RestApi {
      * @return ApiResponse&lt;Void&gt;
      * @throws ApiException transport exception
      */
-    @Path("/v1/{accountId}/drafts/{draftId}/documents/{documentId}/content/decrypted")
+    @Path("/v1/{accountId}/drafts/{draftId}/documents/{documentId}/decrypted-content")
     @PUT
     @Consumes("application/json; charset=utf-8")
     public ApiResponse<Void> updateDecryptedDocumentContent(
@@ -350,7 +350,7 @@ public class DraftsApi extends RestApi {
      * @return ApiResponse&lt;String&gt;
      * @throws ApiException transport exception
      */
-    @Path("/v1/{accountId}/drafts/{draftId}/documents/{documentId}/content/encrypted")
+    @Path("/v1/{accountId}/drafts/{draftId}/documents/{documentId}/encrypted-content")
     @GET
     @Consumes("application/json; charset=utf-8")
     public ApiResponse<String> getEncryptedDocumentContent(@PathParam("accountId") String accountId,
