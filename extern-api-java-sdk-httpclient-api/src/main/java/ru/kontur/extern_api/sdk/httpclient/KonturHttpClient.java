2d7c068d3f6b5b320734a9a9b78ab5050f2e55b8
BrittleFoot <i_o@rtural.ru>
2019-04-22 18:31:17 +0500

    fix NPE in httpclient and authbuilders

--- extern-api-java-sdk-httpclient-api/src/main/java/ru/kontur/extern_api/sdk/httpclient/KonturHttpClient.java
+++ extern-api-java-sdk-httpclient-api/src/main/java/ru/kontur/extern_api/sdk/httpclient/KonturHttpClient.java
@@ -32,7 +32,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 import okhttp3.Call;
 import okhttp3.Headers;
 import okhttp3.HttpUrl;
@@ -51,7 +50,6 @@ import ru.kontur.extern_api.sdk.SerializationProvider;
 import ru.kontur.extern_api.sdk.adaptor.ApiException;
 import ru.kontur.extern_api.sdk.adaptor.ApiResponse;
 import ru.kontur.extern_api.sdk.adaptor.HttpClient;
-import ru.kontur.extern_api.sdk.provider.UserAgentProvider;
 
 public class KonturHttpClient implements HttpClient {
 
@@ -67,7 +65,6 @@ public class KonturHttpClient implements HttpClient {
     private SerializationProvider gsonProvider;
     private final LibapiResponseConverter responseConverter;
 
-    private UserAgentProvider userAgentProvider;
     private String serviceBaseUri;
 
     public KonturHttpClient(KonturConfiguredClient client, GsonProvider gsonProvider) {
@@ -99,27 +96,6 @@ public class KonturHttpClient implements HttpClient {
     }
 
     @Override
-    public void setConnectWaiting(int millisecond) {
-        client.setConnectTimeout(millisecond, TimeUnit.MILLISECONDS);
-    }
-
-    @Override
-    public void setReadTimeout(int millisecond) {
-        client.setReadTimeout(millisecond, TimeUnit.MILLISECONDS);
-    }
-
-    @Override
-    public HttpClient setUserAgentProvider(UserAgentProvider userAgentProvider) {
-        this.userAgentProvider = userAgentProvider;
-        return this;
-    }
-
-    @Override
-    public UserAgentProvider getUserAgentProvider() {
-        return userAgentProvider;
-    }
-
-    @Override
     public <T> ApiResponse<T> submitHttpRequest(
             String httpRequestUri,
             String httpMethod,
@@ -136,12 +112,6 @@ public class KonturHttpClient implements HttpClient {
         if (headerParams == null) {
             headerParams = Collections.emptyMap();
         }
-
-        client.setUserAgentSupplier(() -> Optional
-                .ofNullable(userAgentProvider)
-                .map(UserAgentProvider::getUserAgent)
-                .orElse(null)
-        );
 
         String urlPath = serviceBaseUri + httpRequestUri;
         Builder queryBuilder = Optional.ofNullable(HttpUrl.parse(urlPath))
