ad91714594971b7b8f5c2ab99aa3c87723e18b3d
BrittleFoot <i_o@rtural.ru>
2019-04-23 14:17:27 +0500

    get rid of vintage in IT

--- extern-api-java-sdk-integrational-tests/src/test/java/ru/kontur/extern_api/sdk/RelatedDocumentsIT.java
+++ extern-api-java-sdk-integrational-tests/src/test/java/ru/kontur/extern_api/sdk/RelatedDocumentsIT.java
@@ -22,11 +22,12 @@
 
 package ru.kontur.extern_api.sdk;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static ru.kontur.extern_api.sdk.utils.TestUtils.fromWin1251Bytes;
 
 import java.util.Arrays;
