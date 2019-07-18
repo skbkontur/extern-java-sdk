ba24d9e9fd95d64eaea2ed62c00dccc13733da17
Michael Vodyanyy <coderliq@gmail.com>
2019-07-18 17:56:49 +0500

    CreateDraft and SignDraft steps in scenario test

--- extern-api-java-sdk-integrational-tests/src/test/java/ru/kontur/extern_api/sdk/scenario/BankCloudDssTestScenario.java
+++ extern-api-java-sdk-integrational-tests/src/test/java/ru/kontur/extern_api/sdk/scenario/BankCloudDssTestScenario.java
@@ -23,8 +23,10 @@
 
 package ru.kontur.extern_api.sdk.scenario;
 
+import java.security.cert.CertificateException;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 import okhttp3.logging.HttpLoggingInterceptor.Level;
 import org.junit.jupiter.api.Assertions;
@@ -34,15 +36,12 @@ import ru.kontur.extern_api.sdk.Configuration;
 import ru.kontur.extern_api.sdk.ExternEngine;
 import ru.kontur.extern_api.sdk.ExternEngineBuilder;
 import ru.kontur.extern_api.sdk.adaptor.ApiException;
-import ru.kontur.extern_api.sdk.adaptor.QueryContext;
+import ru.kontur.extern_api.sdk.crypt.CryptoApi;
 import ru.kontur.extern_api.sdk.model.*;
-import ru.kontur.extern_api.sdk.model.PrepareResult.Status;
 import ru.kontur.extern_api.sdk.utils.ApproveCodeProvider;
 import ru.kontur.extern_api.sdk.utils.PreparedTestData;
 import ru.kontur.extern_api.sdk.utils.TestConfig;
 import ru.kontur.extern_api.sdk.utils.TestSuite;
-import ru.kontur.extern_api.sdk.utils.UncheckedRunnable;
-import ru.kontur.extern_api.sdk.utils.Zip;
 
 
 class BankCloudDssTestScenario {
@@ -50,13 +49,15 @@ class BankCloudDssTestScenario {
     private static ExternEngine engine;
     private static Certificate senderCertificate;
     private static TestSuite test;
+    private static Configuration configuration;
+    private static CryptoApi cryptoApi;
 
     @BeforeAll
-    static void setUpClass() {
+    static void setUpClass() throws CertificateException {
 
         String dssConfigPath = "/secret/extern-sdk-dss-config.json";
 
-        Configuration configuration = TestConfig.LoadConfigFromEnvironment(dssConfigPath);
+        configuration = TestConfig.LoadConfigFromEnvironment(dssConfigPath);
 
         engine = ExternEngineBuilder.createExternEngine(configuration.getServiceBaseUri())
                 .apiKey(configuration.getApiKey())
@@ -66,7 +67,9 @@ class BankCloudDssTestScenario {
                 )
                 .doNotUseCryptoProvider()
                 .accountId(configuration.getAccountId())
-                .build(Level.BASIC);
+                .build(Level.BODY);
+
+        cryptoApi = new CryptoApi();
     }
 
     @Test
@@ -79,10 +82,126 @@ class BankCloudDssTestScenario {
         }
     }
 
-    void dssScenario() {
+    void dssScenario() throws Exception {
 
-        List<Certificate> cert = engine.getCertificateService().getCertificates(0, 10).join().getOrThrow()
-                .getCertificates();
-        System.out.println(cert.get(0).getFio());
+        senderCertificate = getDssCert();
+        System.out.printf(
+                "Using certificate: fio = %s, inn = %s, kpp = %s\n",
+                senderCertificate.getFio(),
+                senderCertificate.getInn(),
+                senderCertificate.getKpp()
+        );
+
+        Account account = getAccount();
+        engine.setAccountProvider(account::getId);
+        System.out.printf(
+                "Using account: %s inn = %s kpp = %s\n",
+                account.getOrganizationName(),
+                account.getInn(),
+                account.getKpp()
+        );
+
+        sendDraftWithUsn(account, senderCertificate);
+    }
+
+    Account getAccount() throws ExecutionException, InterruptedException {
+
+        return engine.getAccountService()
+                .getAccountsAsync(0, 100)
+                .get()
+                .getOrThrow()
+                .getAccounts()
+                .stream()
+                .filter(acc -> acc.getInn().equals(senderCertificate.getInn()))
+                .collect(Collectors.toList())
+                .get(0);
+    }
+
+    private void sendDraftWithUsn(Account senderAcc, Certificate certificate)
+            throws Exception {
+
+        SenderRequest sender = new SenderRequest(
+                senderAcc.getInn(),
+                senderAcc.getKpp(),
+                senderCertificate.getContent(),
+                "8.8.8.8"
+        );
+
+        Recipient recipient = new FnsRecipient("0087");
+
+        OrganizationRequest oPayer = new OrganizationRequest(
+                senderAcc.getInn(),
+                senderAcc.getKpp(),
+                senderAcc.getOrganizationName()
+        );
+
+        UUID draftId = engine.getDraftService()
+                .createAsync(sender, recipient, oPayer)
+                .get()
+                .getOrThrow();
+
+        System.out.println("Draft created");
+
+        UsnServiceContractInfo usn = PreparedTestData.usnV2(certificate, oPayer);
+
+        DraftDocument document = engine.getDraftService()
+                .createAndBuildDeclarationAsync(draftId, 2, usn)
+                .get()
+                .getOrThrow();
+
+        System.out.println("Usn document built and added to draft");
+
+        openDraftDocumentAsPdf(draftId, document.getId());
+        cloudSignDraftWithDssCert(draftId);
+    }
+
+    private void openDraftDocumentAsPdf(UUID draftId, UUID documentId) throws Exception {
+        engine.getDraftService()
+                .getDocumentAsPdfAsync(draftId, documentId)
+                .get()
+                .getOrThrow();
+    }
+
+    private void cloudSignDraftWithDssCert(UUID draftId) throws Exception {
+
+        ApproveCodeProvider smsProvider = new ApproveCodeProvider(engine);
+
+        SignInitiation signInitiation = engine.getDraftService()
+                .cloudSignInitAsync(draftId)
+                .get()
+                .getOrThrow();
+
+        assert (signInitiation.getConfirmType() == ConfirmType.MY_DSS);
+
+        TaskState taskState;
+        do {
+            TaskInfo ti = new TaskInfo();
+            ti.setId(UUID.fromString(signInitiation.getTaskId()));
+
+            taskState = engine.getTaskService(draftId).getTaskStatus(ti).get();
+            Thread.sleep(1000);
+
+        } while (taskState == TaskState.RUNNING);
+
+        engine.getDraftService().lookupAsync(draftId).join().getOrThrow().getDocuments()
+                .stream()
+                .map(link -> engine.getHttpClient().followGetLink(link.getHref(), DraftDocument.class))
+                .map(draftDocument -> engine.getDraftService().getSignatureContentAsync(draftId,draftDocument.getId()).join().getOrThrow())
+                .forEach(signature -> Assertions.assertTrue(signature.length > 0));
+        System.out.println("Draft was successfully signed");
+    }
+
+    private Certificate getDssCert() throws Exception {
+
+        return engine
+                .getCertificateService()
+                .getCertificates(0, 100)
+                .get()
+                .getOrThrow()
+                .getCertificates()
+                .stream()
+                .filter(c -> cryptoApi.getThumbprint(c.getContent()).equals(configuration.getThumbprint()))
+                .findFirst()
+                .get();
     }
 }
