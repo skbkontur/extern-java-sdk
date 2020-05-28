95308dc66bc6577bccd43bf2aa7d3ca9deeb292f
Dmitry Babenkov <babenkov_ds@skbkontur.ru>
2020-05-28 13:28:05 +0400

    refactoring and cleanup test

--- extern-api-java-sdk-integrational-tests/src/test/java/ru/kontur/extern_api/sdk/scenario/PfrReportV2CloudTestScenario.java
+++ extern-api-java-sdk-integrational-tests/src/test/java/ru/kontur/extern_api/sdk/scenario/PfrReportV2CloudTestScenario.java
@@ -26,7 +26,6 @@ package ru.kontur.extern_api.sdk.scenario;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -45,29 +44,25 @@ import ru.kontur.extern_api.sdk.model.Certificate;
 import ru.kontur.extern_api.sdk.model.Docflow;
 import ru.kontur.extern_api.sdk.model.DocflowStatus;
 import ru.kontur.extern_api.sdk.model.Document;
-import ru.kontur.extern_api.sdk.model.DocumentContents;
 import ru.kontur.extern_api.sdk.model.Draft;
 import ru.kontur.extern_api.sdk.model.DraftDocument;
 import ru.kontur.extern_api.sdk.model.Link;
-import ru.kontur.extern_api.sdk.model.SignInitiation;
 import ru.kontur.extern_api.sdk.model.SignedDraft;
 import ru.kontur.extern_api.sdk.model.builders.BuildDraftsBuilderResult;
 import ru.kontur.extern_api.sdk.model.builders.pfr_report.PfrReportDraftsBuilder;
 import ru.kontur.extern_api.sdk.model.builders.pfr_report.PfrReportDraftsBuilderDocument;
-import ru.kontur.extern_api.sdk.model.pfr.PfrReply;
-import ru.kontur.extern_api.sdk.model.pfr.PfrReplyDocument;
 import ru.kontur.extern_api.sdk.service.DraftService;
 import ru.kontur.extern_api.sdk.service.builders.pfr_report.PfrReportDraftsBuilderService;
 import ru.kontur.extern_api.sdk.utils.ApproveCodeProvider;
 import ru.kontur.extern_api.sdk.utils.Awaiter;
-import ru.kontur.extern_api.sdk.utils.CryptoUtils;
 import ru.kontur.extern_api.sdk.utils.TestSuite;
-import ru.kontur.extern_api.sdk.utils.UncheckedRunnable;
-import ru.kontur.extern_api.sdk.utils.Zip;
 import ru.kontur.extern_api.sdk.utils.builders.DraftsBuilderCreator;
 import ru.kontur.extern_api.sdk.utils.builders.DraftsBuilderDocumentCreator;
 import ru.kontur.extern_api.sdk.utils.builders.DraftsBuilderDocumentFileCreator;
 
+/**
+ * Тест на сценарий отправки отчета PfrV2 СЗВ-ТД
+ */
 @Execution(ExecutionMode.CONCURRENT)
 class PfrReportV2CloudTestScenario {
 
@@ -126,7 +121,7 @@ class PfrReportV2CloudTestScenario {
                 account.getKpp()
         );
 
-        Docflow pfrDocflow = sendPfrReport(engine, account, workingCert).get().getOrThrow();
+        Docflow pfrDocflow = sendPfrReport(engine, workingCert).get().getOrThrow();
         System.out.println("Draft is sent. Long live the Docflow " + pfrDocflow.getId());
 
         finishDocflow(pfrDocflow);
@@ -172,38 +167,13 @@ class PfrReportV2CloudTestScenario {
 
     private CompletableFuture<QueryContext<Docflow>> sendPfrReport(
             ExternEngine engine,
-            Account account,
             Certificate workingCert
     ) throws Exception {
         UUID draftId = buildPfrDraftViaBuilder(engine, workingCert);
         DraftService draftService = engine.getDraftService();
-        Draft draft = draftService
-                .lookupAsync(draftId)
-                .join()
-                .getOrThrow();
-
-        HttpClient httpClient = engine.getHttpClient();
-
-        /*
-        draft.getDocuments()
-                .stream()
-                .map(Link::getHref)
-                .map(link -> httpClient.followGetLink(link, DraftDocument.class))
-                .map(document -> {
-                    DocumentContents contents = updateSignaturesInDraftDocument(
-                            engine,
-                            draftService,
-                            draftId,
-                            httpClient,
-                            document
-                    );
-
-                    return contents;
-                })
-                .toArray();
-         */
 
         // check pdf printing
+        HttpClient httpClient = engine.getHttpClient();
         Draft updatedDraft = draftService
                 .lookupAsync(draftId)
                 .join()
@@ -275,9 +245,6 @@ class PfrReportV2CloudTestScenario {
         ).thenApply(QueryContext::getOrThrow);
     }
 
-    /**
-     * Создаёт и отправляет ответные документы до завершения ДО если нужно.
-     */
     private void finishDocflow(Docflow docflow) throws Exception {
 
         int budget = 60 * 5_000;
@@ -295,37 +262,19 @@ class PfrReportV2CloudTestScenario {
         docflow = updated;
         Assertions.assertNotNull(docflow, "Cannot get docflow in 5 minutes");
 
-        while (true) {
-            System.out.println("Docflow status: " + docflow.getStatus());
+        System.out.println("Docflow status: " + docflow.getStatus());
 
-            if (docflow.getStatus() == DocflowStatus.SENT) {
-                System.out.println("Docflow " + docflow.getId() + " status: " + docflow.getStatus());
-                break;
-            }
-
-            if (docflow.getStatus() == DocflowStatus.FINISHED) {
-                System.out.println("Docflow " + docflow.getId() + " status: " + docflow.getStatus());
-                break;
-            }
-
-            if (docflow.getStatus() == DocflowStatus.RESPONSE_ARRIVED) {
-                System.out.println("Docflow " + docflow.getId() + "status: " + docflow.getStatus());
-            }
-
-            Document document = docflow.getDocuments().stream()
-                    //.filter(x -> x.getDescription().getType().toString() == "")
-                    .findFirst()
-                    .orElse(null);
-
-            try {
-                openDocflowDocumentAsPdf(docflow.getId(), document.getId());
-            } catch (ApiException e) {
-                System.out.println(
-                        "Ok, Cannot print document " + document.getDescription().getType() + ". " + e
-                                .getMessage());
-            }
+        if (docflow.getStatus() == DocflowStatus.SENT) {
+            System.out.println("Docflow " + docflow.getId() + " status: " + docflow.getStatus());
         }
 
+        if (docflow.getStatus() == DocflowStatus.FINISHED) {
+            System.out.println("Docflow " + docflow.getId() + " status: " + docflow.getStatus());
+        }
+
+        if (docflow.getStatus() == DocflowStatus.RESPONSE_ARRIVED) {
+            System.out.println("Docflow " + docflow.getId() + "status: " + docflow.getStatus());
+        }
     }
 
     private void openDraftDocumentAsPdf(UUID draftId, DraftDocument draftDocument) throws Exception {
@@ -345,47 +294,6 @@ class PfrReportV2CloudTestScenario {
         }
     }
 
-    private void openDocflowDocumentAsPdf(UUID docflowId, UUID documentId) throws Exception {
-
-        byte[] document = downloadDocumentContent(docflowId, documentId);
-
-        if (document == null) {
-            System.out.println("Cannot show the document");
-            return;
-        }
-
-        engine.getDocflowService()
-                .getDocumentAsPdfAsync(docflowId, documentId, document)
-                .get()
-                .getOrThrow();
-    }
-
-    private byte[] downloadDocumentContent(UUID docflowId, UUID documentId) throws Exception {
-        Document document = engine.getDocflowService()
-                .lookupDocumentAsync(docflowId, documentId)
-                .get()
-                .getOrThrow();
-
-        /* TODO - почему падает на unzip - или контент не сжат или зашифрован но флаг hasDecryptedContent стоит true?
-        if (document.hasDecryptedContent()) {
-            System.out.println("Document is already decrypted");
-            byte[] content = engine.getDocflowService()
-                    .getDecryptedContentAsync(docflowId, documentId)
-                    .get()
-                    .getOrThrow();
-
-            if (document.getDescription().getCompressed()) {
-                content = Zip.unzip(content);
-            }
-
-            return content;
-        }*/
-
-        System.out.println("Decrypting document...");
-        return cloudDecryptDocument(docflowId, documentId);
-    }
-
-    // TODO добавить когда будет готова поддержва пфр cloudSign
     private void cloudSignDraft(UUID draftId) throws Exception {
 
         ApproveCodeProvider smsProvider = new ApproveCodeProvider(engine);
@@ -399,19 +307,6 @@ class PfrReportV2CloudTestScenario {
                 "Draft signed in cloud, %s document(s) signed\n",
                 signedDraft.getSignedDocuments().size()
         );
-    }
-
-
-    private byte[] cloudDecryptDocument(UUID docflowId, UUID documentId) {
-        ApproveCodeProvider smsProvider = new ApproveCodeProvider(engine);
-
-        return engine.getDocflowService()
-                .cloudDecryptDocument(
-                        docflowId.toString(),
-                        documentId.toString(),
-                        senderCertificate.getContent(),
-                        init -> smsProvider.apply(init.getRequestId())
-                ).getOrThrow();
     }
 
     private List<Certificate> findWorkingCerts() throws Exception {
