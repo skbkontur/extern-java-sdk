2a2dda22217c33fe2b56a0be678abcfb0d00c8ab
d.alexandrov <d.alexandrov@skbkontur.ru>
2020-06-23 12:09:27 +0500

    review fixes

--- extern-api-java-sdk-common/src/main/java/ru/kontur/extern_api/sdk/model/pfr/Zped/InsurerIpDto.java
+++ extern-api-java-sdk-common/src/main/java/ru/kontur/extern_api/sdk/model/pfr/Zped/InsurerIpDto.java
@@ -1,10 +1,31 @@
 package ru.kontur.extern_api.sdk.model.pfr.Zped;
 
+/**
+ * Класс для передачи информации об ИП
+ */
 public class InsurerIpDto{
+    /**
+     * Имя
+     */
     public String Name;
+    /**
+     * Фамилия
+     */
     public String Surname;
+    /**
+     * Отчество
+     */
     public String Patronymic;
+    /**
+     * СНИЛС
+     */
     public String Snils;
+    /**
+     * Номер телефона
+     */
     public String Phone;
+    /**
+     * Адрес электронной почты
+     */
     public String Email;
 }
--- extern-api-java-sdk-common/src/main/java/ru/kontur/extern_api/sdk/model/pfr/Zped/RepresentativeDto.java
+++ extern-api-java-sdk-common/src/main/java/ru/kontur/extern_api/sdk/model/pfr/Zped/RepresentativeDto.java
@@ -1,8 +1,20 @@
 package ru.kontur.extern_api.sdk.model.pfr.Zped;
 
 public class RepresentativeDto{
+    /**
+     * Имя
+     */
     public String Name;
+    /**
+     * Фамилия
+     */
     public String Surname;
+    /**
+     * Отчество
+     */
     public String Patronymic;
+    /**
+     * Должность
+     */
     public String Position;
 }
