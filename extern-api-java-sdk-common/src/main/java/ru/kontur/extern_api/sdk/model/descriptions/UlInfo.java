0a575def485149542ed6ae00be973386bce665a8
am.volkov <am.volkov@skbkontur.ru>
2023-11-16 18:49:15 +0300

    Добавил ogrn в UlInfo

--- extern-api-java-sdk-common/src/main/java/ru/kontur/extern_api/sdk/model/descriptions/UlInfo.java
+++ extern-api-java-sdk-common/src/main/java/ru/kontur/extern_api/sdk/model/descriptions/UlInfo.java
@@ -4,11 +4,21 @@ public class UlInfo {
 
     private String name;
 
+    private String ogrn;
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getOgrn() {
+        return ogrn;
+    }
+
+    public void setOgrn(String ogrn) {
+        this.ogrn = ogrn;
     }
 }
